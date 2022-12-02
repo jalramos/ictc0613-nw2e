0becc191c3f10a632442fbfa1d28cb9c3f146f8c
talentojv <116441989+talentojv@users.noreply.github.com>
2022-12-02 17:12:29 +0800

    Add files via upload

---
+++ Act5_MyLinkedList_Talento_NW2E.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package act5_mylinkedlist_talento_nw2e;
+
+
+import java.util.*;
+
+public class Act5_MyLinkedList_Talento_NW2E {
+            
+        
+
+    public String FirstName, MiddleName,LastName,AreaCode, TelephoneNumber,Gender;
+    public int Age;
+    public Act5_MyLinkedList_Talento_NW2E  next;
+    public static Act5_MyLinkedList_Talento_NW2E firstNode;
+    public static Act5_MyLinkedList_Talento_NW2E lastNode = null;
+    
+    public  Act5_MyLinkedList_Talento_NW2E  (String fname,String mname,String lname,String acode,String telnum,String gender,int age, Act5_MyLinkedList_Talento_NW2E  n){
+      FirstName = fname;
+      MiddleName = mname;
+      LastName = lname;1
+      AreaCode = acode;
+      TelephoneNumber = telnum;
+      Gender = gender;
+      Age = age;
+      next =  n;
+    }
+    
+    public static void main(String[] args) {
+       
+       int numnodes;
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many member's information will be entered? ");
+        numnodes = input.nextInt();
+        for(int i = 0; i < numnodes; i++){
+            System.out.println("\nKindly give the information of member #" + (i+1));
+            Information();
+        }
+             Act5_MyLinkedList_Talento_NW2E  n = firstNode;
+            
+            while(n != null){
+                System.out.println("\nWelcome to the club " + n.FirstName + " " + n.MiddleName + " " + n.LastName + "!");
+                System.out.println("Your area code and telephone number is (" + n.AreaCode + ")" + n.TelephoneNumber + ".");
+                System.out.println("You are a " + n.Gender + " member, and your age is " + n.Age + ".");
+                n = n.next;
+                
+            }
+
+}
+
+   static void Information()
+{
+         Scanner input = new Scanner(System.in);
+ String FName, MName, LName, ACode, TNum, gender;
+        int  age;
+     
+            System.out.print("Enter First Name: ");
+            FName = input.nextLine();
+            System.out.print("Enter Middle Name: ");
+            MName = input.nextLine();
+            System.out.print("Enter Last Name: ");
+            LName = input.nextLine();
+            System.out.print("Enter Area Code: ");
+            ACode = input.nextLine();
+            System.out.print("Enter Telephone Number: ");           
+            TNum = input.nextLine();
+            System.out.print("Enter Gender: ");
+            gender = input.nextLine();
+            System.out.print("Enter Age: ");
+            age = input.nextInt();
+
+   Act5_MyLinkedList_Talento_NW2E  n = new  Act5_MyLinkedList_Talento_NW2E (FName, MName, LName, ACode, TNum, gender, age, null);
+            if(lastNode !=null){
+                lastNode.next = n;
+                lastNode = n;
+            }
+            else {
+                firstNode = n;
+                lastNode = n;
+            }
+        }
+
+}
